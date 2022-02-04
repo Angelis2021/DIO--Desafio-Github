39becc023b9f51ef89e08f60ec49da6e61d3bb69
Marcela de Angelis <86483928+Angelis2021@users.noreply.github.com>
2022-02-03 22:51:50 -0300

    Clone_Netflix
    Clone da página da Netflix. Projeto desenvolvido durante o treinamento DIO.

---
+++ Projeto_Netflix/assests/Js/setup.js
@@ -0,0 +1,16 @@
+$('.owl-carousel').owlCarousel({ //o $ permite configurar os elementos em tela com o jquery//
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
